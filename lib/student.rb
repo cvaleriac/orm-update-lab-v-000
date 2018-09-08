27b2cc75b2d149cc4db4cbb04ad557d69c627b5f
valeria campilongo <cvaleriac@hotmail.it>
2018-09-08 23:02:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,20 +42,23 @@ def self.create_table
   end
 end
 
-def self.create(name, grade)
+def self.create(name:, grade:)
   student = Student.new(name, grade)
-  student.save
-  student
+   student.save
+   student
 end
 
 def self.new_from_db(row)
-     Student.new(row[0], row[1], row[2])
-  end
+  id = row[0]
+  name = row[1]
+  grade = row[2]
+  self.new(id, name, grade)
+end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new_from_db(result)
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+  result = DB[:conn].execute(sql, name)[0]
+  Student.new(result[0], result[1], result[2])
   end
 
   def update
